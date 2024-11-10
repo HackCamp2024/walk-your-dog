5681ef29e02958f26378d5f45662326b7714f106
tracyw04 <tracywang41@gmail.com>
2024-11-10 11:00:55 -0800

    Add Hearts Component

---
+++ src/components/Hearts/Hearts.jsx
@@ -0,0 +1,15 @@
+import { AiTwotoneHeart } from "react-icons/ai";
+
+const Hearts = () => {
+    return (
+        <div className="image-row">
+            <AiTwotoneHeart className="first-heart" />
+            <AiTwotoneHeart className="second-heart" />
+            <AiTwotoneHeart className="third-heart" />
+            <AiTwotoneHeart className="fourth-heart" />
+            <AiTwotoneHeart className="fifth-heart" />
+        </div>
+    );
+}
+
+export default Hearts;
