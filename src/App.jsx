8fc3419253e64a453cff08c4c358e3a5b2a42c30
Lada496 <67321065+Lada496@users.noreply.github.com>
2024-11-10 13:39:51 -0800

    add update next day status

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
+import { Header } from "./components/Header/Header";
 import { Progress } from "./components/Progress/Progress";
 
 import "./App.css";
 
 function App() {
+  const [hearts, setHearts] = useState(3);
+
+  // TODO: integrate with Input later
+  const [steps, setSteps] = useState(5000);
+
+  // value for the demo
+  // how many days passed from the init state
+  const [day, setDay] = useState(0);
+
+  // For demo
+  const handleClickNextDay = () => {
+    setDay(day + 1);
+    setSteps(0);
+    // TODO change the number of hearts to fill depending on the number of steps the user had at the day
+  };
+
   return (
     <>
-      <Progress steps={5000} />
+      <Header
+        day={day}
+        handleClickNextDay={handleClickNextDay}
+        hearts={hearts}
+      />
+      <Progress steps={steps} />
     </>
   );
 }
