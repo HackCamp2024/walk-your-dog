ed7f9a15b4c9dea7c3465e08491902add83746dc
tracyw04 <tracywang41@gmail.com>
2024-11-10 17:10:21 -0800

    camera bug

--- src/components/AR/AugReality.jsx
+++ src/components/AR/AugReality.jsx
@@ -1,46 +1,46 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
 const ARView = () => {
-  const [isCameraActive, setIsCameraActive] = useState(false);
+  const [isARActive, setIsARActive] = useState(false);
 
   const handleStartAR = () => {
-    setIsCameraActive(true);
+    setIsARActive(true);
   };
 
   const handleStopAR = () => {
-    setIsCameraActive(false);
+    setIsARActive(false);
   };
 
   return (
     <div>
+      {/* Button to start AR */}
       <button onClick={handleStartAR}>Start AR</button>
-      {isCameraActive && (
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
-          <a-scene
-            embedded
-            arjs="sourceType: webcam; debugUIEnabled: false; trackingMethod: best;"
-            style={{ width: "100%", height: "100%" }}
-          >
-            {/* Marker for AR */}
-            <a-marker preset="hiro">
-              {/* 3D Dog Image */}
-              <a-image
-                src="/assets/happy-running-dog.png"
-                scale="2 2 2"
-                position="0 0 0"
-                rotation="0 0 0"
-              ></a-image>
-            </a-marker>
 
-            {/* Camera Entity */}
-            <a-entity camera></a-entity>
-          </a-scene>
-        </div>
+      {/* AR Scene: Display when AR is active */}
+      {isARActive && (
+        <a-scene
+          embedded
+          arjs="sourceType: webcam; debugUIEnabled: false;"
+          style={{ width: '100%', height: '100%' }}
+        >
+          {/* Fixed Position Image (displayed in front of the user) */}
+          <a-entity position="0 1 -3">
+            <a-image
+              src="/assets/happy-running-dog.png" // Path to your PNG
+              scale="2 2 2"
+              rotation="0 0 0"
+            ></a-image>
+          </a-entity>
+
+          {/* Camera for AR */}
+          <a-entity camera></a-entity>
+        </a-scene>
       )}
-      {isCameraActive && <button onClick={handleStopAR}>Stop AR</button>}
+
+      {/* Button to stop AR */}
+      {isARActive && <button onClick={handleStopAR}>Stop AR</button>}
     </div>
   );
 };
 
 export default ARView;
-
