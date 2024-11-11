c1eb15e68c10d9449e63e7c922728bb2bae10ec9
tracyw04 <tracywang41@gmail.com>
2024-11-10 17:04:26 -0800

    Update ARJSX

--- src/components/AR/AugReality.jsx
+++ src/components/AR/AugReality.jsx
@@ -1,47 +1,46 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const ARView = () => {
-    const [isCameraActive, setIsCameraActive] = useState(false);
+  const [isCameraActive, setIsCameraActive] = useState(false);
 
-    const handleStartAR = () => {
-        setIsCameraActive(true);
-    };
+  const handleStartAR = () => {
+    setIsCameraActive(true);
+  };
 
-    const handleStopAR = () => {
-        setIsCameraActive(false);
-    };
+  const handleStopAR = () => {
+    setIsCameraActive(false);
+  };
 
-    return (
-        <div>
-            {/* Button to start AR */}
-            <button onClick={handleStartAR}>Start AR</button>
+  return (
+    <div>
+      <button onClick={handleStartAR}>Start AR</button>
+      {isCameraActive && (
+        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+          <a-scene
+            embedded
+            arjs="sourceType: webcam; debugUIEnabled: false; trackingMethod: best;"
+            style={{ width: "100%", height: "100%" }}
+          >
+            {/* Marker for AR */}
+            <a-marker preset="hiro">
+              {/* 3D Dog Image */}
+              <a-image
+                src="/assets/happy-running-dog.png"
+                scale="2 2 2"
+                position="0 0 0"
+                rotation="0 0 0"
+              ></a-image>
+            </a-marker>
 
-            {/* AR Scene that appears when camera is active */}
-            {isCameraActive && (
-                <div style={{ position: "relative", width: "100%", height: "400px" }}>
-                    <a-scene
-                        embedded
-                        arjs="sourceType: webcam; debugUIEnabled: false; trackingMethod: best;"
-                        style={{ width: "100%", height: "100%" }}
-                    >
-                        <a-marker preset="hiro">
-                            <a-image
-                                src="/assets/happy-running-dog.png"
-                                scale="2 2 2"
-                                position="0 0 0"
-                                rotation="0 0 0"
-                            ></a-image>
-                        </a-marker>
-
-                        <a-entity camera></a-entity>
-                    </a-scene>
-                </div>
-            )}
-
-            {/* Button to stop AR */}
-            {isCameraActive && <button onClick={handleStopAR}>Stop AR</button>}
+            {/* Camera Entity */}
+            <a-entity camera></a-entity>
+          </a-scene>
         </div>
-    );
+      )}
+      {isCameraActive && <button onClick={handleStopAR}>Stop AR</button>}
+    </div>
+  );
 };
 
 export default ARView;
+
