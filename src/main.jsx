44968aa02cd80e547b0e1668abc37998cab74f97
Lada496 <67321065+Lada496@users.noreply.github.com>
2024-11-12 03:12:35 -0800

    try text

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+  <GoogleOAuthProvider clientId="847989147030-asdd2k80un0c9i5j96091ae4ct2v8or1.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>
 );
